eba3835d65cf3426f085ff4a7957088628b04d17
akinba <akinba@gmail.com>
2016-11-19 20:17:22 +0200

    OK

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use( express.static("public"));
 var db = new seq("postgres://postgres:pi@www.akinba.com:5432/edremit");
 
 var ip=process.env.IP||'localhost';
-var port=process.env.POR||3003
+var port=process.env.PORT||3003
 app.listen( 3003,ip, ()=>{
 	console.log(`Sunucu ${ip}:${port}'de calisiyor`  );
 	//console.log(db.Sequelize);
