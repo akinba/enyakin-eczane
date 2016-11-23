f90e05bd644b1c93cd036f582ec0490c9ade5ccc
SERKAN AKIN <serkan.akin@netcad.com>
2016-11-23 20:08:34 +0300

    add socket.io

--- app.js
+++ app.js
@@ -2,23 +2,88 @@ const express	= require("express");
 const app		= express();
 const bodyparser = require("body-parser");
 const pg		= require("pg");
-const seq		= require("sequelize");
+const sequelize	= require("sequelize");
 const moment	= require("moment");
 const morgan	= require("morgan");
+const socketOI	= require("socket.io");
+const http		= require("http");
+var port=process.env.POR||3003
 
-
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 app.set("view engine", "ejs");
 app.use( express.static("public"));
 
+var server 	= http.createServer(app);
+var io 		= socketOI(server);
 
-var db = new seq("postgres://postgres:pi@www.akinba.com:5432/edremit");
+var db = new sequelize("postgres://postgres:pi@www.akinba.com:5432/enyakin");
+var eczane = db.define('eczane', 
+{
+	gid: {
+		type: sequelize.UUID,
+		primaryKey: true,
+		defaultValue: sequelize.UUIDV4
+	},
+	adi: {
+		type: sequelize.STRING
+	},
+	adres: {
+		type: sequelize.STRING
+	},
+	adres_tarifi: {
+		type: sequelize.STRING
+	},
+	telefon: {
+		type: sequelize.STRING
+	},
+	fax: {
+		type: sequelize.STRING
+	},
+	nobet_tarihi: {
+		type: sequelize.STRING(10)
+	},
+	il: {
+		type: sequelize.STRING(20)
+	},
+	ilce: {
+		type: sequelize.STRING(20)
+	},
+	enlem: {
+		type: sequelize.FLOAT
+	},
+	boylam: {
+		type: sequelize.FLOAT
+	},
+	konum: {
+		type: sequelize.STRING
+	},
+	geom: {
+		type: sequelize.GEOMETRY('POINT', 4326)
+	}
+}, 
 
-var ip=process.env.IP||'localhost';
-var port=process.env.POR||3003
-app.listen( 3003,ip, ()=>{
-	console.log(`Sunucu ${ip}:${port}'de calisiyor`  );
+{
+  freezeTableName: true
+});
+
+io.on ('connection', (socket)=>{
+	console.log(socket.id+' baglandi');
+	socket.emit('layer',{}
+
+	);
+
+});
+
+
+app.get('/', (req,res)=>{
+	res.render('index');
+});
+
+
+
+server.listen( 3003, ()=>{
+	console.log(`Sunucu ${port}'de calisiyor`  );
 	//console.log(db.Sequelize);
 });
